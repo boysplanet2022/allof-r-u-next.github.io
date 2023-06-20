6fce3a15839a9c60c04a4c58acd546897911b2c5
jihyungato <amayonoseppun@gmail.com>
2023-06-19 18:43:20 -0700

    Initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -87,7 +87,7 @@ function readFromCSV(path) {
       trainee.grade = traineeArray[4];
       trainee.birthyear = traineeArray[5];
       trainee.eliminated = traineeArray[6] === 'e'; // sets trainee to be eliminated if 'e' appears in 6th col
-      trainee.top7 = traineeArray[6] === 't'; // sets trainee to top 12 if 't' appears in 6th column
+      trainee.top7 = traineeArray[6] === 't'; // sets trainee to top 7 if 't' appears in 6th column
       trainee.id = parseInt(traineeArray[7]) - 1; // trainee id is the original ordering of the trainees in the first csv
       trainee.image =
         trainee.name_romanized.replace(" ", "").replace("-", "") + ".jpg";
@@ -111,7 +111,7 @@ function readFromCSV(path) {
   // Constructor for a blank ranking list
   function newRanking() {
     // holds the ordered list of rankings that the user selects
-    let ranking = new Array(07);
+    let ranking = new Array(7);
     for (let i = 0; i < ranking.length; i++) {
       ranking[i] = newTrainee();
     }
